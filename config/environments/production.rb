92710038dc95611a82a5d1fff2eeaaf4f4bc70d7
Michael Ruml <ruml@mail.muni.cz>
2016-01-29 23:13:26 +0100

    Configure SendGrid on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mail system
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'huebook.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address    => "smtp.sendgrid.net",
+    :port       => 25,
+    :user_name  => ENV['SENDGRID_USERNAME'],
+    :password   => ENV['SENDGRID_PASSWORD'],
+    :domain     => ENV['SENDGRID_DOMAIN'],
+    :authentication  => :plain,
+    :enable_starttls_auto => true
+  }    
 end
