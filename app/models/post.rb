0d704505dda050e8954469021e1d7e1fd18864fb
Michael Ruml <ruml@mail.muni.cz>
2016-01-30 00:20:32 +0100

    Try to fix PSQL migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
     belongs_to :user
-    belongs_to :orig_post, class_name: "Post"
+    belongs_to :orig_post, class_name: "Post", foreign_key: "orig_post_id", primary_key: "id"
     belongs_to :wall, polymorphic: true
-    has_many :shares, class_name: "Post", foreign_key: "orig_post_id"
+    has_many :shares, class_name: "Post", foreign_key: "orig_post_id", primary_key: "id"
     has_many :comments
     has_many :likes, as: :object
 
