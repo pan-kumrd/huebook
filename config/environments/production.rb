8e36b15477418339433c92743c5397b07edbd001
Michael Ruml <ruml@mail.muni.cz>
2016-01-30 19:10:55 +0100

    Try now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,13 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  
+  class NoCompression
+    def compress(string)
+      # do nothing
+      string
+    end
+  end
   config.assets.compress = true
   config.assets.js_compressor = NoCompression.new
   config.assets.css_compressor = NoCompression.new
