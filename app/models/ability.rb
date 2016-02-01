6330163a8845689bd1c5b93211117e425dbc1fca
Michael Ruml <ruml@mail.muni.cz>
2016-02-01 16:17:52 +0100

    Enhance security model of the API

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,19 @@ class Ability
 
   def initialize(user)
     if user
-      can :manage, :all
+      can [ :destroy ], Comment, :user_id == user.id
+      can [ :destroy ], Event, :organizer_id == user.id
+      can [ :update, :destroy ], Friendship do |f|
+        f.friend_id == user.id || f.initiator_id == user.id
+      end
+      can [ :destroy ], Message do |m|
+        f.sender_id == user.id || f.to_id == user.id
+      end
+      can [ :destroy ], Like, :user_id == user.id
+      can [ :destroy ], Post, :user_id == user.id
+      can [ :update, :destroy ], EventRsvp, :user_id == user.id
+      can [ :update, :destroy ], User, :id == user.id
+      can :read, :all
     end
   end
 end
